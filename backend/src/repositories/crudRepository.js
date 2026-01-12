82ca9cb03245cdd7c16fc3c6489ba02c758fe4cc
ypramod25 <py999618@gmail.com>
2026-01-12 12:54:16 +0530

    added crud repositories

---
+++ backend/src/repositories/crudRepository.js
@@ -0,0 +1,24 @@
+export default function crudRepository(model) {
+    return {
+        create: async function (data) {
+            const newDoc = await model.create(data);
+            return newDoc
+        },
+        getAll: async function () {
+            const allDocs = await model.find();
+            return allDocs;
+        },
+        getById: async function (id) {
+            const doc = await model.findById(id);
+            return doc;
+        },
+        delete: async function () {
+            const responce = await model.findByIdAndDelete(id);
+            return response;
+        },
+        update: async function (id, data) {
+            const updatedDoc = await model.findByIdAndDelete(id, data, {new: true});
+            return updatedDoc;
+        }
+    }
+}
---
+++ backend/src/repositories/userRepository.js
@@ -0,0 +1,18 @@
+import User from '../schema/user.js'
+import crudRepository from './crudRepository.js'
+
+const userRepository = {
+    ...crudRepository(User),
+
+    getByEmail: async function (email) {
+        const user = await User.findOne({email});
+        return user;
+    }, 
+
+    getByUsername: async function (name) {
+        const user = await User.findOne({username: name});
+        return user;
+    }
+};
+
+export default userRepository;
