b9f811b3f32703b128e7db73574c0b79e91b100a
ypramod25 <py999618@gmail.com>
2026-01-12 10:31:03 +0530

    added basic routing layer

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,11 +3,13 @@ import { StatusCodes } from 'http-status-codes';
 
 import connectDB from './config/dbConfig.js';
 import { PORT } from './config/serverConfig.js';
+import apiRouter from './routing/apiRouter.js';
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));// to parse form data {extended: true} to parse nested objects
+app.use(express.urlencoded({ extended: true }));// to parse form data {extended} to parse nested objects
+app.use('/api', apiRouter);
 
 app.get('/ping', (req, res) => {
   return res.status(StatusCodes.OK).json({ message: 'pong' });
---
+++ backend/src/routing/v1/v1Router.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+import userRouter from "./users.js";
+
+const router = express.Router();
+
+router.use('/users', userRouter);
+
+export default router;
