3419f4b15eae4276d3ff7f606217cea35ff533d6
ypramod25 <py999618@gmail.com>
2026-05-30 15:30:12 +0530

    mailQueue producer file fixed

--- backend/src/producers/mailQueueProducers.js
+++ backend/src/producers/mailQueueProducers.js
@@ -1,14 +1,5 @@
 import mailQueue from "../queues/mailQueue.js";
 
-// export const addEmailToMailQueue = async (emailData) => {
-//     console.log('Initiating email sending process');
-//     try {
-//         await mailQueue.add(emailData);
-//         console.log('Email added to mail queue:', emailData);
-//     } catch (error) {
-//         console.error('Error adding email to mail queue:', error);
-//     }
-// };
 export const addEmailToMailQueue = async (emailData) => {
     console.log('Initiating email sending process');
     try {
